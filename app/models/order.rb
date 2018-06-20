f08bdc793bc23339017fc70c490e828b42881957
Esperanza11 <eruribe@miuandes.cl>
2018-06-20 17:11:00 -0400

    validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,8 @@ class Order < ApplicationRecord
   has_many :order_pizzas
   has_many :pizzas, through: :order_pizzas
   has_many :delivery_infos
+  validates :payment_method, presence: true
+  validates :user, presence: true
   validates :name, presence: true
-  validates :name, presence: true
-  validates :name, presence: true
+  validates_associated :pizzas
 end
